37d188c610a0d4f5c8d0c754a67b40047e02d154
ashley631 <thesavannahchic@gmail.com>
2018-10-22 23:26:55 -0400

    <new commit>

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).on("click", ".cell", (event) => {
+    $(event.target).addClass(event)
+
+});
+
+
+$( "#target" ).click(function() {
+    alert( "Handler for .click() called." );
+  });
+
+
